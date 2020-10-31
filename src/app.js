cdcbbced9d833a5b1d10455050c2892b5be8151c
Shola Bioku <bilushonline@gmail.com>
2020-10-31 17:01:15 +0100

    Add morgan to log if env is developmen

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
 
 import productRoutes from './routes/products';
 import orderRoutes from './routes/orders';
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
