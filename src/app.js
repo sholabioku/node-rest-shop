32a4797ebc68fa53f02ddf8abb90fb0bd1e7e0a3
Shola Bioku <bilushonline@gmail.com>
2020-10-31 21:36:21 +0100

    Use cors for all routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { urlencoded, json } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import productRoutes from './routes/products';
 import orderRoutes from './routes/orders';
@@ -11,6 +12,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(urlencoded({ extended: false }));
 app.use(json());
+app.use(cors());
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
