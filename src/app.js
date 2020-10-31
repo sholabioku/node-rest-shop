461569292080c8a312ac493c3cf5193f4e9e6f3a
Shola Bioku <bilushonline@gmail.com>
2020-10-31 15:56:34 +0100

    Add CRUD functionality for /orders endpoint with dummy data

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import productRoutes from './routes/products';
+import orderRoutes from './routes/orders';
 
 const app = express();
 
 app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
 
 export default app;
