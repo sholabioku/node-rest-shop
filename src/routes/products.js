ff7db3330d097175bb8229987b6532bfb8976ae6
Shola Bioku <bilushonline@gmail.com>
2020-11-03 09:51:54 +0100

    Add CRUD endpoint functionality for products

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,49 +1,93 @@
 import { Router } from 'express';
+import mongoose from 'mongoose';
+import _ from 'lodash';
+
+import Product from '../models/product';
 
 const router = Router();
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requets for /products',
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-  };
-  res.status(201).json({
-    message: 'Handling POST requets for /products',
-    createdProduct: product,
   });
+
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Handling POST requets for /products',
+        createdProduct: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
 
-  if (id === 'specila') {
-    res.status(200).json({
-      message: 'You discovered the special id',
-      id,
+  Product.findById(id)
+    .then((doc) => {
+      if (doc) {
+        console.log(doc);
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'No valid enrty found for the provided ID' });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  } else {
-    res.status(200).json({
-      message: 'You passed an ID',
-    });
-  }
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product',
-  });
+  const id = req.params.productId;
+  const body = _.pick(req.body, ['name', 'price']);
+  Product.updateOne({ _id: id }, { $set: body }, { new: true })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product',
-  });
+  const id = req.params.productId;
+  Product.deleteMany({ _id: id })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 export default router;
