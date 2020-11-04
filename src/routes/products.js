67028083cadc03e3f78e60e0aad86d4bef5b7d49
Shola Bioku <bilushonline@gmail.com>
2020-11-04 08:40:45 +0100

    Add mongoose validation

--- src/routes/products.js
+++ src/routes/products.js
@@ -10,8 +10,22 @@ const router = Router();
 router.get(
   '/',
   asyncHandler(async (req, res, next) => {
-    const docs = await Product.find();
-    res.status(200).json(docs);
+    const docs = await Product.find().select('name price _id');
+    const response = {
+      count: docs.length,
+      products: docs.map((doc) => {
+        return {
+          name: doc.name,
+          product: doc.price,
+          _id: doc._id,
+          request: {
+            type: 'GET',
+            url: `http://localhost:3000/products/${doc._id}`,
+          },
+        };
+      }),
+    };
+    res.status(200).json(response);
   })
 );
 
@@ -24,8 +38,16 @@ router.post(
 
     const result = await product.save();
     res.status(201).json({
-      message: 'Handling POST requets for /products',
-      createdProduct: result,
+      message: 'Product created successfully',
+      createdProduct: {
+        name: result.name,
+        price: result.price,
+        _id: result._id,
+        request: {
+          type: 'GET',
+          url: `http://localhost:3000/products/${result._id}`,
+        },
+      },
     });
   })
 );
@@ -38,14 +60,20 @@ router.get(
       return res.status(404).json({ message: 'Invalid ID' });
     }
 
-    const doc = await Product.findById(id);
+    const doc = await Product.findById(id).select('name price _id');
     if (!doc) {
       return res
         .status(404)
         .json({ message: 'No valid enrty found for the provided ID' });
     }
 
-    res.status(200).json(doc);
+    res.status(200).json({
+      product: doc,
+      request: {
+        type: 'GET',
+        url: 'http://localhost:3000/products',
+      },
+    });
   })
 );
 
@@ -55,13 +83,15 @@ router.patch(
     const id = req.params.productId;
     const body = _.pick(req.body, ['name', 'price']);
 
-    const result = await Product.updateOne(
-      { _id: id },
-      { $set: body },
-      { new: true }
-    );
+    await Product.updateOne({ _id: id }, { $set: body }, { new: true });
 
-    res.status(200).json(result);
+    res.status(200).json({
+      message: 'Product updated successfully',
+      request: {
+        type: 'GET',
+        url: `http://localhost:3000/products/${id}`,
+      },
+    });
   })
 );
 
@@ -69,9 +99,16 @@ router.delete(
   '/:productId',
   asyncHandler(async (req, res, next) => {
     const id = req.params.productId;
-    const result = await Product.deleteMany({ _id: id });
+    await Product.deleteMany({ _id: id });
 
-    res.status(200).json(result);
+    res.status(200).json({
+      message: 'Product deleted',
+      request: {
+        type: 'POST',
+        url: 'http://localhost:3000/products',
+        body: { name: 'String', price: 'Number' },
+      },
+    });
   })
 );
 
