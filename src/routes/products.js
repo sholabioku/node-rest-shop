a7a38b89c1e69dbf541e58de8ea1a0470181d2b6
Shola Bioku <bilushonline@gmail.com>
2020-11-05 08:55:20 +0100

    Add filter for file type

--- src/routes/products.js
+++ src/routes/products.js
@@ -17,7 +17,21 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
 
 router.get(
   '/',
