333777391476f17d4b5b0f2e23791c9fbf10a437
sholabioku <bilushonline@gmail.com>
2020-11-15 14:53:40 +0100

    Validate object ids

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -4,18 +4,18 @@ import Product from '../models/product';
 import asyncHandler from '../middlewares/async';
 
 export const getProducts = asyncHandler(async (req, res, next) => {
-  const docs = await Product.find().select('name price productImage _id');
+  const products = await Product.find().select('name price productImage _id');
   const response = {
-    count: docs.length,
-    products: docs.map((doc) => {
+    count: products.length,
+    products: products.map((product) => {
       return {
-        name: doc.name,
-        price: doc.price,
-        productImage: doc.productImage,
-        _id: doc._id,
+        name: product.name,
+        price: product.price,
+        productImage: product.productImage,
+        _id: product._id,
         request: {
           type: 'GET',
-          url: `http://localhost:3000/products/${doc._id}`,
+          url: `http://localhost:3000/products/${product._id}`,
         },
       };
     }),
@@ -30,17 +30,17 @@ export const addProduct = asyncHandler(async (req, res, next) => {
     productImage: req.file.path,
   });
 
-  const result = await product.save();
+  const createdProduct = await product.save();
   res.status(201).json({
     message: 'Product created successfully',
     createdProduct: {
-      name: result.name,
-      price: result.price,
-      productImage: result.productImage,
-      _id: result._id,
+      name: createdProduct.name,
+      price: createdProduct.price,
+      productImage: createdProduct.productImage,
+      _id: createdProduct._id,
       request: {
         type: 'GET',
-        url: `http://localhost:3000/products/${result._id}`,
+        url: `http://localhost:3000/products/${createdProduct._id}`,
       },
     },
   });
@@ -48,19 +48,17 @@ export const addProduct = asyncHandler(async (req, res, next) => {
 
 export const getProduct = asyncHandler(async (req, res, next) => {
   const id = req.params.productId;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).json({ message: 'Invalid ID' });
-  }
 
-  const doc = await Product.findById(id).select('name price productImage _id');
-  if (!doc) {
-    return res
-      .status(404)
-      .json({ message: 'No valid enrty found for the provided ID' });
-  }
+  const product = await Product.findById(id).select(
+    'name price productImage _id'
+  );
+
+  if (!product) return res.status(404).json({ message: 'No product found' });
 
   res.status(200).json({
-    product: doc,
+    product,
     request: {
       type: 'GET',
       url: 'http://localhost:3000/products',
@@ -70,9 +68,17 @@ export const getProduct = asyncHandler(async (req, res, next) => {
 
 export const editProduct = asyncHandler(async (req, res, next) => {
   const id = req.params.productId;
-  const body = _.pick(req.body, ['name', 'price']);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ message: 'Invalid ID' });
 
-  await Product.updateOne({ _id: id }, { $set: body }, { new: true });
+  const body = _.pick(req.body, ['name', 'price']);
+  const product = await Product.findOneAndUpdate(
+    { _id: id },
+    { $set: body },
+    { new: true }
+  );
+
+  if (!product) return res.status(404).json({ message: 'No product found' });
 
   res.status(200).json({
     message: 'Product updated successfully',
@@ -85,7 +91,11 @@ export const editProduct = asyncHandler(async (req, res, next) => {
 
 export const deleteProduct = asyncHandler(async (req, res, next) => {
   const id = req.params.productId;
-  await Product.deleteMany({ _id: id });
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({ message: 'Invalid ID' });
+
+  const product = await Product.findOneAndDelete({ _id: id });
+  if (!product) return res.status(404).json({ message: 'No product found' });
 
   res.status(200).json({
     message: 'Product deleted',
