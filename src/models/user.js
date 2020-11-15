91a7836a760399ba3ff6dc2012816b7b28bdb796
sholabioku <bilushonline@gmail.com>
2020-11-15 17:54:46 +0100

    Add built in validation

--- src/models/user.js
+++ src/models/user.js
@@ -6,7 +6,6 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     unique: true,
-    match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
     required: true,
   },
   password: {
