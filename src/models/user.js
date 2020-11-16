47a8b6391336c4a53e58d6260555eb77bfeb2511
sholabioku <bilushonline@gmail.com>
2020-11-16 13:41:16 +0100

    Email validation

--- src/models/user.js
+++ src/models/user.js
@@ -6,10 +6,12 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
     unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
     required: true,
   },
   password: {
     type: String,
+    minlength: 5,
     required: true,
   },
 });
