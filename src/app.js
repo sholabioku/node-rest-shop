13c68ffec0912144f1216f3e85b160dd7ab49147
sholabioku <bilushonline@gmail.com>
2020-11-21 14:19:21 +0100

    Install hpp

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 1,
+  max: 100,
 });
 
 app.use('/uploads', express.static('src/uploads'));
