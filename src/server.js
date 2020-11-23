d383b86444e026ddcaeb83c1f264f348cfc69b67
sholabioku <bilushonline@gmail.com>
2020-11-23 14:10:33 +0100

    export server

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ import http from 'http';
 import colors from 'colors';
 import app from './app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const server = http.createServer(app);
 
 server.listen(port, () =>
   console.log(`Server up and running on port ${port}`.yellow.bold.underline)
 );
+
+export default server;
 
 // "env": {
 //     "MONGO_URI": "mongodb+srv://bilush:noderestshop@node-rest-shop.2fiwx.mongodb.net/ShopApi?retryWrites=true&w=majority",
