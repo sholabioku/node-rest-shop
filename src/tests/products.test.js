a15df6f216ffb26e6aff95cffe0741481002771e
sholabioku <bilushonline@gmail.com>
2020-11-25 17:32:47 +0100

    refracture products.test.js

--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -2,30 +2,15 @@ import expect from 'expect';
 import request from 'supertest';
 
 import server from '../server';
-import Product from '../models/product';
+import { products, populateProducts } from './seed/seed';
 
 describe('Integration Test for Product', () => {
+  beforeEach(populateProducts);
   describe('GET /products', () => {
-    const products = [
-      {
-        name: 'productOne',
-        price: 2000,
-        productImage: 'benz.png',
-      },
-      {
-        name: 'productTwo',
-        price: 1000,
-        productImage: 'laptop.png',
-      },
-    ];
-
     it('should return all products', async () => {
-      await Product.collection.insertMany(products);
-
       const res = await request(server).get('/products');
       expect(res.status).toBe(200);
       expect(res.body.products.length).toBe(2);
-      await Product.collection.deleteMany({});
     });
   });
 });
