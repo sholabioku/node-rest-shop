54ce76cff41c087c7d8a5a665dabb853271d60f4
sholabioku <bilushonline@gmail.com>
2020-11-27 07:48:21 +0100

    Write test for GET /products/:productId to return a product if valid productId is passed

--- src/tests/products.test.js
+++ src/tests/products.test.js
@@ -2,15 +2,25 @@ import expect from 'expect';
 import request from 'supertest';
 
 import server from '../server';
+import Product from '../models/product';
 import { products, populateProducts } from './seed/seed';
 
 describe('Integration Test for Product', () => {
-  beforeEach(populateProducts);
   describe('GET /products', () => {
+    beforeEach(populateProducts);
     it('should return all products', async () => {
       const res = await request(server).get('/products');
       expect(res.status).toBe(200);
       expect(res.body.products.length).toBe(2);
+    });
+  });
+
+  describe('GET /products/:productId', () => {
+    it('should return a product if valid id is passed', async () => {
+      const res = await request(server).get(`/products/${products[0]._id}`);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('product.name', products[0].name);
+      console.log(res.body);
     });
   });
 });
