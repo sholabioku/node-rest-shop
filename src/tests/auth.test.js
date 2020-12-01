28b96fdd1014a63410017fd612dcd5d35b3cf367
sholabioku <bilushonline@gmail.com>
2020-12-01 18:15:48 +0100

    write test to return 400 if invalid token is provided

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -10,11 +10,15 @@ import { products, populateProducts } from './seed/seed';
 describe('check-auth middlewrae', () => {
   beforeEach(populateProducts);
   it('should return 401 if no token is provided', async () => {
+    const res = await request(server).post('/products').send(products[0]);
+    expect(res.status).toBe(401);
+  });
+  it('should return 400 if invalid token is provided', async () => {
     // const token = new User().generateAuthToken();
     const res = await request(server)
       .post('/products')
-      // .set('auth', null)
+      .set('auth', 'a')
       .send(products[0]);
-    expect(res.status).toBe(401);
+    expect(res.status).toBe(400);
   });
 });
