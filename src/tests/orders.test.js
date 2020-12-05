b8abfb037fa417052ebfe1c60f05486d7407a67d
sholabioku <bilushonline@gmail.com>
2020-12-05 12:34:51 +0100

    Write integration test for GET /orders/:orderId

--- src/tests/orders.test.js
+++ src/tests/orders.test.js
@@ -4,8 +4,8 @@ import request from 'supertest';
 
 import server from '../server';
 import User from '../models/user';
-import Product from '../models/product';
 import Order from '../models/order';
+import Product from '../models/product';
 
 describe('Integration test for orders routes', () => {
   describe('GET /orders', () => {
@@ -38,6 +38,44 @@ describe('Integration test for orders routes', () => {
     it('should return 401 if client is not logged in', async () => {
       const res = await request(server).get('/orders/:orderId');
       expect(res.status).toBe(401);
+    });
+
+    it('should return order if valid id is passed', async () => {
+      const product = new Product({
+        _id: mongoose.Types.ObjectId(),
+        name: 'Benz',
+        price: 1200,
+        productImage: 'benz.png',
+      });
+      await product.save();
+
+      const order = new Order({
+        product,
+        quantity: 2,
+        customer: mongoose.Types.ObjectId(),
+      });
+
+      await order.save();
+      const token = new User().generateAuthToken();
+      const res = await request(server)
+        .get(`/orders/${order._id}`)
+        .set('auth', token);
+      expect(res.status).toBe(200);
+    });
+
+    it('should return 404 if invalid id is passed', async () => {
+      const token = new User().generateAuthToken();
+      const res = await request(server)
+        .get('/orders/123abc')
+        .set('auth', token);
+      expect(res.status).toBe(404);
+    });
+
+    it('should return 404 if id passed does not has order', async () => {
+      const token = new User().generateAuthToken();
+      const id = new mongoose.Types.ObjectId();
+      const res = await request(server).get(`/orders/${id}`).set('auth', token);
+      expect(res.status).toBe(404);
     });
   });
 });
